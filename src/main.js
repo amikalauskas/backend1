228bb710333f075a7e1de9be507572e0698935d9
amikalauskas <andriusminadvirati@gmail.com>
2022-09-12 15:40:00 +0300

    feat: created project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-console.log('vyksta kodsas serverio puseje');
+const path = require('path');
+const fs = require('fs');
+
+const fileNames = ['pirmas.txt', 'antras.json', 'trecias.csv'];
+
+fileNames.forEach((fileName) => {
+  const filepath = path.join(__dirname, __filename);
+  fs.writeFile(filepath, 'testas', err);
+  if (err) {
+    console.error(err);
+  }
+});
+
+console.log(__dirname);
+console.log(__filename);
